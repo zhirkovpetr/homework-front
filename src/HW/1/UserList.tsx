72feb877815050bed5f1f7ba6f32ab1c26d9e680
Petr <zhirkovpetr95@gmail.com>
2023-08-15 22:51:55 +0300

    refactor: refactor users map in UserList

--- src/HW/1/UserList.tsx
+++ src/HW/1/UserList.tsx
@@ -18,17 +18,16 @@ export const UserList = (props: UserListPropsType) => {
   return (
     <div id={'hw01-users'}>
       <h2>User List:</h2>
-      {props.users.map((user) => {
-        return (
-          <ul>
+      <ul>
+        {props.users.map((user) => {
+          return (
             <li key={user.id} id={`hw01-user-${user.id}`}>
               <strong>{user.name}</strong> (Age: {user.age})<strong> Address:</strong>
               {user.address.street}, {user.address.city}
             </li>
-          </ul>
-        )
-      })}
-
+          )
+        })}
+      </ul>
     </div>
   );
 };
