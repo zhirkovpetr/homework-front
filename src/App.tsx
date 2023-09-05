3aeb1a615774bef3fbcb384c9a20f0a321657451
Petr <zhirkovpetr95@gmail.com>
2023-09-05 17:28:29 +0300

    feat: add homework 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
-import {HW1} from './HW/1/HW1';
-import {HW2} from './HW/2/HW2';
-import {HW3} from './HW/3/HW3';
+//import {HW1} from './HW/1/HW1';
+//import {HW2} from './HW/2/HW2';
+//import {HW3} from './HW/3/HW3';
 import {HW4} from './HW/4/HW4';
 
 function App() {
@@ -9,8 +9,8 @@ function App() {
     <div className="App">
      {/* <HW1/>*/}
       {/*<HW2 />*/}
-      <HW3 />
-      {/*<HW4 />*/}
+      {/*<HW3 />*/}
+      <HW4 />
     </div>
   );
 }
